281e7c76796b675b2b37b8fcb39828cc70d5d61f
Jonah Schneider <Redflag11104@gmail.com>
2026-02-26 13:10:27 -0500

    Import pandas and read multiple sales data files

--- process_sales.py
+++ process_sales.py
@@ -1,1 +1,6 @@
+import pandas as pd
+import os
 
+file1 = pd.read_csv("data/daily_sales_data_0.csv")
+file2 = pd.read_csv("data/daily_sales_data_1.csv")
+file3 = pd.read_csv("data/daily_sales_data_2.csv")
