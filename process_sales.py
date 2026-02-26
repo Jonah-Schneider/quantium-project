21559ee419432d95ab80d26fa3639795b5f154c5
Jonah Schneider <Redflag11104@gmail.com>
2026-02-26 15:14:28 -0500

    Fix product name filter for pink morsels

--- process_sales.py
+++ process_sales.py
@@ -6,7 +6,7 @@ file2 = pd.read_csv("data/daily_sales_data_1.csv")
 file3 = pd.read_csv("data/daily_sales_data_2.csv")
 combined_file = pd.concat([file1, file2, file3])
 
-pink_only_file = combined_file[combined_file["product"] == "Pink Morsels"]
+pink_only_file = combined_file[combined_file["product"] == "pink Morsel"]
 pink_only_file["sales"] = pink_only_file["quantity"] * pink_only_file["price"]
 final_file = pink_only_file[["sales", "date", "region"]]
 final_file.to_csv("output.csv", index=False)
