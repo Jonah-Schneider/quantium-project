cfd5ee72ead298aea3c2f959b36f40cb53624e75
Jonah Schneider <Redflag11104@gmail.com>
2026-02-26 15:03:53 -0500

    Combine sales data and filter for Pink Morsels

--- process_sales.py
+++ process_sales.py
@@ -4,3 +4,7 @@ import os
 file1 = pd.read_csv("data/daily_sales_data_0.csv")
 file2 = pd.read_csv("data/daily_sales_data_1.csv")
 file3 = pd.read_csv("data/daily_sales_data_2.csv")
+combined_file = pd.concat([file1, file2, file3])
+
+pink_only_file = combined_file[combined_file["product"] == "Pink Morsels"]
+
