4cee0a0322933b6418b843da3c73d149ad9c769c
Jonah Schneider <Redflag11104@gmail.com>
2026-03-06 15:30:24 -0500

    Create test function for Dash app header
    Added initial test function for header verification.

--- test_001.py
+++ test_001.py
@@ -1,3 +1,4 @@
 #Your task is to create a test suite to verify your Dash app is working as expected. Using the standard Dash testing framework (documentation linked in the resources below) write three tests which ensure the following:
 #The header is present.
-
+from app import *
+def test_heading ()
