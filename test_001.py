b2230458c52b448364b03083b7e6365a99ff9481
Jonah Schneider <Redflag11104@gmail.com>
2026-03-06 13:20:42 -0500

    Document test suite requirements in test_001.py
    Add comments outlining test suite requirements for Dash app.

--- test_001.py
+++ test_001.py
@@ -1,1 +1,3 @@
+#Your task is to create a test suite to verify your Dash app is working as expected. Using the standard Dash testing framework (documentation linked in the resources below) write three tests which ensure the following:
+#The header is present.
 
